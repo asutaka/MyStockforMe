eb38d587a393f0aaf02e4d9ba36943d204215f7d
PhuNV <phunv@vatgia.com>
2021-02-23 16:47:55 +0700

    add lib

---
+++ MyStock/Common/StaticValue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MyStock.Common
+{
+    public static class StaticValue
+    {
+        public static Dictionary<long, string>  dicCoPhieu = new Dictionary<long,string>();
+    }
+}
---
+++ MyStock/InitProgram.cs
@@ -0,0 +1,32 @@
+﻿using MyStock.Common;
+using MyStock.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStock
+{
+    public class InitProgram
+    {
+        public InitProgram()
+        {
+            InitStaticValue();
+            //SqlServer.ImportCsvFolder("C:\\");
+            //Jobs
+            //new JobManager().ExecuteAllJobs();
+        }
+        private void InitStaticValue()
+        {
+            var dt = SqlServer.GetData("select ID,MaChungKhoan from CoPhieu");
+            foreach (DataRow item in dt.Rows)
+            {
+                var key = long.Parse(item["ID"].ToString());
+                var value = item["MaChungKhoan"].ToString();
+                StaticValue.dicCoPhieu.Add(key,value);
+            }
+        }
+    }
+}
